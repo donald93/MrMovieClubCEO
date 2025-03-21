c3769bf2e82a7ea0aa9c08900c6f3aba5098c828
donald93 <blargalicious@gmail.com>
2025-03-20 20:41:26 -0700

    Initial commit of a technically working bot

---
+++ MrMovieClubCEO/Puzzle.cs
@@ -0,0 +1,8 @@
+﻿namespace MrMovieClubCEO;
+
+public record Puzzle
+{
+    public required string Id { get; set; }
+    public required string Question { get; set; }
+    public required IReadOnlyCollection<string> Answers { get; set; }
+}
